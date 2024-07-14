f51ea8a6c91945374ccc7ed256f2c3c701ad39b0
EMS engineering <j39g8bxh@gmail.com>
2024-07-14 17:38:27 +0900

    [update] #2 cesiumのviewが描画されるように変更

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,56 +1,31 @@
-import { json, redirect, LoaderFunctionArgs } from "@remix-run/node";
-import {
-  Form,
-  Link,
-  Links,
-  Meta,
-  NavLink,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-  useNavigation,
-  useSubmit,
-} from "@remix-run/react";
-import { useEffect } from "react";
+import { json, LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { Links, Meta, Scripts, ScrollRestoration } from '@remix-run/react';
 
-import { createEmptyContact, getContacts } from "./data";
+import { createEmptyContact, getContacts } from './data';
 
 export const action = async () => {
   const contact = await createEmptyContact();
   return redirect(`/contacts/${contact.id}/edit`);
 };
 
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction } from '@remix-run/node';
 
-import appStylesHref from "./app.scss?url";
+import * as Cesium from 'cesium';
+import 'cesium/Build/Cesium/Widgets/widgets.css'; // 必須
+import { Entity, Viewer } from 'resium';
+import appStylesHref from './app.scss?url';
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: appStylesHref },
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: appStylesHref }];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
-  const q = url.searchParams.get("q");
+  const q = url.searchParams.get('q');
   const contacts = await getContacts(q);
   return json({ contacts, q });
 };
 
 export default function App() {
-  const { contacts, q } = useLoaderData<typeof loader>();
-  const navigation = useNavigation();
-  const submit = useSubmit();
-
-  const searching =
-    navigation.location &&
-    new URLSearchParams(navigation.location.search).has("q");
-
-  useEffect(() => {
-    const searchField = document.getElementById("q");
-    if (searchField instanceof HTMLInputElement) {
-      searchField.value = q || "";
-    }
-  }, [q]);
+  Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN;
 
   return (
     <html lang="en">
@@ -61,74 +36,17 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form
-              id="search-form"
-              onChange={(event) => {
-                const isFirstSearch = q === null;
-                submit(event.currentTarget, {
-                  replace: !isFirstSearch,
-                });
-              }}
-              role="search"
-            >
-              <input
-                id="q"
-                aria-label="Search contacts"
-                className={searching ? "loading" : ""}
-                defaultValue={q || ""}
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={!searching} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <NavLink
-                      className={({ isActive, isPending }) =>
-                        isActive ? "active" : isPending ? "pending" : ""
-                      }
-                      to={`contacts/${contact.id}`}
-                    >
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? <span>★</span> : null}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
-
-        <div
-          className={
-            navigation.state === "loading" && !searching ? "loading" : ""
-          }
-          id="detail"
-        >
+        <Viewer full>
+          <Entity
+            description="test"
+            name="tokyo"
+            point={{ pixelSize: 10 }}
+            position={Cesium.Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
+          />
+        </Viewer>
+        {/* <div id="detail">
           <Outlet />
-        </div>
-
+        </div> */}
         <ScrollRestoration />
         <Scripts />
       </body>
